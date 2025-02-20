a359b64673df1a014d36cc4b97ceafbbe0e30b51
nkgrnkgr <beefupme@gmail.com>
2025-02-20 09:50:22 +0900

    コメント追加

--- src/app/use-hooks/_components/playground.tsx
+++ src/app/use-hooks/_components/playground.tsx
@@ -3,6 +3,10 @@
 import { useState } from "react";
 import type { Todo } from "../page";
 
+// use の hooks を使ってみましょう
+// サーバー側で use hooks を使ってみましょう
+// この記事を一緒に読みましょう
+// https://nextjs.org/docs/app/building-your-application/upgrading/single-page-applications
 export function UsePlayground({ todo }: { todo: Todo }) {
   const [count, setCount] = useState(0);
   return (
